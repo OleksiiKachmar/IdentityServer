4f2b47ac32c5b6da58d73ed7108f510a92c63689
RomanKarpinskyy <roman.karpinskyy@118118money.com>
2019-09-02 18:05:20 +0300

    Initial Gateway commit
    Initial files + configuration for identity server + auth controller

---
+++ GatewayAPI/GatewayAPI/GatewayAPI/Configuration/IdentityServerConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GatewayAPI.Configuration
+{
+    public class IdentityServerConfig
+    {
+        public string Url { get; set; }
+    }
+}
---
+++ GatewayAPI/GatewayAPI/GatewayAPI/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GatewayAPI.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace GatewayAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IOptions<IdentityServerConfig> _identityServerConfig;
+
+        public AuthController(IOptions<IdentityServerConfig> identityServerConfig)
+        {
+            _identityServerConfig = identityServerConfig;
+        }
+
+        [HttpPost("{token}")]
+        public IActionResult Authenticate()
+        {
+            return StatusCode(200);
+        }
+    }
+}
